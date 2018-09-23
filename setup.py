ce1e009b56192ba57c0d9968a3cc100d26e06b92
Unknown <npraiwan@outlook.com>
2018-09-24 02:14:11 +0700

    add pusher s3

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kpc_connector_utils',
+    packages=find_packages(),
+    version='0.1.5',
+    description='Connector utils and configs',
+    author='Praiwan N.',
+    author_email='npraiwan@outlook.com',
+    url='https://github.com/praiwann/kpc-connector-utils',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'boto3>=1.0.0',
+    ],
+)
