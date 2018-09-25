4281c3326c225b1f3f2ce1231f89dac9d86153d2
Unknown <npraiwan@outlook.com>
2018-09-25 18:03:42 +0700

    update version and remove boto3 from pre-install

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='kpc_connector_utils',
     packages=find_packages(),
-    version='0.1.9',
+    version='0.1.10',
     description='Connector utils and configs',
     author='Praiwan N.',
     author_email='npraiwan@outlook.com',
@@ -12,8 +12,5 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3'
-    ],
-    install_requires=[
-        'boto3>=1.0.0',
-    ],
+    ]
 )
